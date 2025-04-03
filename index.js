1171862d2c63b80bcd94560c6d88430762bd6c45
Sampath- IT Professional <sampathitprofessional@gmail.com>
2025-04-03 09:57:19 +0530

    Update index.js
    SAMHello world edited

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const router = express.Router();
 
 // Hello World for index page
 app.get('/', function (req, res) {
-    return res.send("Welcome to Hello World!");
+    return res.send("Welcome to SAMHello World!");
 })
 
 app.get('/api', function (req, res) {
